e0479d78023f680acbb35c84678f9cfffd29e943
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-08-01 08:12:54 +0300

    Update

--- UserWPFDataBase/DataBase.cs
+++ UserWPFDataBase/DataBase.cs
@@ -20,9 +20,8 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace WpfApp2
 {
     [AddINotifyPropertyChangedInterface]
-    internal class DataBase 
+    internal class DataBase
     {
-        private bool flag = true;
         private readonly string phonePattern = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
         private readonly string loginPattern = @"^[a-zA-Z][a-zA-Z0-9]{3,9}$";
         private readonly string passwordPattern = @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
@@ -31,18 +30,76 @@ namespace WpfApp2
         private readonly string cmd = "select* from Users;select* from Positions;";
         private readonly RelayCommand delete;
         private bool Updated { get; set; }
+        private bool cancelDelete = false;
 
-        private void dataChanged(object sender,DataRowChangeEventArgs e)
+        private void rowDeleted(object sender, DataRowChangeEventArgs e)
+        {
+
+            if (cancelDelete)
+            {
+                e.Row.RejectChanges();
+                return;
+            }
+
+            adapter?.Update(this.dataSet);
+
+        }
+
+        private void dataChanged(object sender, DataRowChangeEventArgs e)
         {
             if (e.Action == DataRowAction.Delete)
             {
                 MessageBoxResult result = MessageBox.Show($"Are you sure you want to delete ?", "Delete", MessageBoxButton.YesNo);
-                if (result == MessageBoxResult.No) return;
+                cancelDelete = result == MessageBoxResult.No;
+                return;
             }
-            else if ((e.Action == DataRowAction.Add || e.Action == DataRowAction.Change) && dataSet != null && dataSet.HasChanges())
-               adapter?.Update(dataSet);
+            else if ((e.Action != DataRowAction.Add && e.Action != DataRowAction.Change) || (this.dataSet != null && !this.dataSet.HasChanges())) return;
+            DataRow? row = e.Row;
+            string? message = null;
+            for (int i = 1; i <= 5; i++)
+            {
+                if (string.IsNullOrWhiteSpace(row?[i].ToString()))
+                {
+                    message = $"{row?.Table.Columns[i].ColumnName} not be empty !!!";
+                    break;
+                }
+                switch (i)
+                {
+                    case 1:
+                        if (!Regex.IsMatch(row?[i]?.ToString() ?? string.Empty, loginPattern))
+                            message = $"Invalid login \"{row?[i]?.ToString()}\" !!!\nAt least one letter or number\nEvery character from the start to the end is a letter or number\nLogin is not allowed to start with digits\nMin/max length restrictions: 3 - 9";
+                        else if (!loginCheck(dataSet, row?[i].ToString()))
+                            message = "This login allready exists !!!";
+                        break;
+                    case 2:
+                        if (!Regex.IsMatch(row?[i]?.ToString() ?? string.Empty, passwordPattern))
+                            message = $"Invalid password \"{row?[i]?.ToString()}\" !!!\nAt least one upper case\nAt least one lower case letter\nAt least one digit\nAt least one special character\nMinimum eight in length 8";
+                        break;
+                    case 3:
+                        if (!Regex.IsMatch(row?[i]?.ToString() ?? string.Empty, phonePattern))
+                            message = $"Invalid phone \"{row?[i]?.ToString()}\"!!!\n(xxx)xxxxxxx\r\n(xxx) xxxxxxx\r\n(xxx)xxx-xxxx\r\n(xxx) xxx-xxxx\r\nxxxxxxxxxx\r\nxxx-xxx-xxxxx"; break;
+                }
+            }
+            if (message != null)
+            {
+                e.Row.RejectChanges();
+                MessageBox.Show(message);
+            }
+            else adapter?.Update(this.dataSet);
+
         }
-        
+
+        private bool loginCheck(DataSet? data, string? login)
+        {
+            int count = 0;
+            foreach (var item in data.Tables[0].Rows)
+            {
+                DataRow? row = (item as DataRowView)?.Row;
+                if (row?["Login"].ToString() == login) count++;
+                if (count > 1) return false;
+            }
+            return true;
+        }
 
 
         private void DeletePositions()
@@ -60,11 +117,13 @@ namespace WpfApp2
                 MessageBoxResult result =  MessageBox.Show($"Are you sure you want to delete {delindexes.Count()} {Positions.ElementAt(PosId)} ?","Delete",MessageBoxButton.YesNo) ;
                 if (result == MessageBoxResult.No) return;
                 dataSet.Tables[0].RowDeleting -= dataChanged;
+                dataSet.Tables[0].RowDeleted -= rowDeleted;
                 deleteAdapter?.Fill(dataSet);
                 foreach (var rowIndex in delindexes)
                     dataSet?.Tables[0]?.Rows.Remove(rowIndex);
                 Updated = true;
                 dataSet.Tables[0].RowDeleting += dataChanged;
+                dataSet.Tables[0].RowDeleted += rowDeleted;
             }
         }
 
@@ -81,13 +140,16 @@ namespace WpfApp2
             _ = adapter?.Fill(dataSet);
             dataSet.Tables[0].RowChanged += new(dataChanged);
             dataSet.Tables[0].RowDeleting += new(dataChanged);
-           Updated = true;
+            dataSet.Tables[0].RowDeleted += new(rowDeleted);
+            Updated = true;
+            Updated = true;
             SelectedIndex = -1;
         }
 
         public bool IsAdmin { get; set; }
         public int PosId { get; set; } = 2;
         public int SelectedIndex { get; set; } = -1;
+
         public DataBase()
         {
             Load();
--- UserWPFDataBase/MainWindow.xaml.cs
+++ UserWPFDataBase/MainWindow.xaml.cs
@@ -23,77 +23,10 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool flag = true;
-        private readonly string phonePattern = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
-        private readonly string loginPattern = @"^[a-zA-Z][a-zA-Z0-9]{3,9}$";
-        private readonly string passwordPattern = @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+      
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void salesGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
-        {
-            if (flag)
-            {
-                DataGrid? dataGrid = sender as DataGrid;
-                DataRow? row = (e.Row.Item as DataRowView)?.Row;
-                string? message = null;
-                flag = false;
-                if (e.EditAction == DataGridEditAction.Cancel) dataGrid?.CancelEdit();
-                else
-                {
-                    for (int i = 1; i <= 5; i++)
-                    {
-                        if (string.IsNullOrWhiteSpace(row?[i].ToString()))
-                        {
-                            message = $"{row?.Table.Columns[i].ColumnName} not be empty !!!";
-                            break;
-                        }
-                        switch (i)
-                        {
-                            case 1:
-                                if (!Regex.IsMatch(row?[i]?.ToString() ?? string.Empty, loginPattern))
-                                    message = $"Invalid login \"{row?[i]?.ToString()}\" !!!\nAt least one letter or number\nEvery character from the start to the end is a letter or number\nLogin is not allowed to start with digits\nMin/max length restrictions: 3 - 9";
-                                else if (!loginCheck(dataGrid, row?[i].ToString()))
-                                    message = "This login allready exists !!!";
-                                break;
-                            case 2:
-                                if (!Regex.IsMatch(row?[i]?.ToString() ?? string.Empty, passwordPattern))
-                                    message = "Invalid password \"{row?[i]?.ToString()}\" !!!\nAt least one upper case\nAt least one lower case letter\nAt least one digit\nAt least one special character\nMinimum eight in length 8";
-                                break;
-                            case 3:
-                                if (!Regex.IsMatch(row?[i]?.ToString() ?? string.Empty, phonePattern))
-                                    message = "Invalid phone \"{row?[i]?.ToString()}\"!!!\n(xxx)xxxxxxx\r\n(xxx) xxxxxxx\r\n(xxx)xxx-xxxx\r\n(xxx) xxx-xxxx\r\nxxxxxxxxxx\r\nxxx-xxx-xxxxx"; break;
-                        }
-                    }
-                }
-
-                if (message != null)
-                {
-                    dataGrid?.CancelEdit();
-                    dataGrid?.Items.Refresh();
-                    MessageBox.Show(message);
-                }
-                else dataGrid?.CommitEdit();
-                flag = true;
-            }
-        }
-
-        private bool loginCheck(DataGrid? data, string? login)
-        {
-            int count = 0;
-            foreach (var item in data.Items)
-            {
-                DataRow? row = (item as DataRowView)?.Row;
-                if (row?["Login"].ToString() == login) count++;
-                if (count > 1) return false;
-            }
-            return true;
-        }
-
-
-
-
     }
 }
